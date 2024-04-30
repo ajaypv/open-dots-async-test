1137fac90fa66c2adbf10800897a6e1c1671c6e7
ajaypv <1nh19cs705ajaypv@gmail.com>
2024-04-30 12:32:14 +0530

    updated with 0.3

--- src/components/danceRoom.jsx
+++ src/components/danceRoom.jsx
@@ -49,7 +49,7 @@ const DanceRoom = () => {
             const currentTime = new Date().getTime();
                 const sessionTime = new Date(session.date).getTime();
                 const timeDifference = (currentTime - sessionTime) / 1000;
-                const timeToSeek = session.timestamp + timeDifference - 0.5;
+                const timeToSeek = session.timestamp + timeDifference +0.3;
                 console.log(timeToSeek);
                 console.log(session.timestamp); 
                 playerRef.pl
